4a2479ca7ef2f2ab96dc10a6b4d98913c2a9bd81
Max <rendon4ik@gmail.com>
2022-06-17 23:09:47 +0300

    Add some clustering methods

--- Visualization/visualization.py
+++ Visualization/visualization.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import plotly.express as px
+from clusterization.clustering import spectral_clustering
 from clusterization.clustersNumber import elbow_method
 from clusterization.clustersNumber import silhouette_method
 from clusterization.clustersNumber import gap_statistics
 from clusterization.clustering import kmeans_method
 from clusterization.clustersNumber import draw_dendogram
-from clusterization.clustering import birch_method
-
+from clusterization.clustering import DBSCAN_method
+from clusterization.clustering import optics_method
+from clusterization.clustering import meanshift_method
 
 data = pd.read_csv('data/encoded_data.csv', encoding='utf-8')
 kmeans_kwargs = {
@@ -17,4 +19,4 @@ kmeans_kwargs = {
     'random_state': 42,
 }
 
-birch_method(data, 4)
+meanshift_method(data)
